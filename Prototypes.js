c035765922f77fb6a26a8423ef961a5a34a0f882
BordenJardine <bordenjardine@gmail.com>
2020-12-18 00:16:02 -0500

    Fix clip selecting bug
    Java was having a hard time figuring out which number the string '3' is supposed to be.

--- Prototypes.js
+++ Prototypes.js
@@ -1738,7 +1738,7 @@ function SessionComponent(name, width, height, trackBank, _colors, mastertrack)
 		{
 			if(self._selectedTrack._clipslots[i].isPlaying)
 			{
-				self._cursorTrack.getClipLauncherSlots().select(i);
+				self._cursorTrack.getClipLauncherSlots().select(Number(i));
 				isPlaying = true;
 				break;
 			}
@@ -1749,7 +1749,7 @@ function SessionComponent(name, width, height, trackBank, _colors, mastertrack)
 			{
 				if(self._selectedTrack._clipslots[i].hasContent)
 				{
-					self._cursorTrack.getClipLauncherSlots().select(i);
+					self._cursorTrack.getClipLauncherSlots().select(Number(i));
 				}
 			}
 		}
